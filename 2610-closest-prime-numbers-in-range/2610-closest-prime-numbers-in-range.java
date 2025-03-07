056640fd2f2bbb003596795e14e22f627524cb48
Anay Patil <73343302+Anay-Patil@users.noreply.github.com>
2025-03-08 00:26:48 +0530

    Time: 100 ms (54%), Space: 50.5 MB (28.8%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,40 +1,43 @@
 class Solution {
-    public int[] closestPrimes(int left, int right) {
-        boolean[] sieve = new boolean[right + 1];
-        Arrays.fill(sieve, true);
-        sieve[0] = sieve[1] = false;
-        
+    private boolean[] sieveHelper(int right) {
+        boolean[] isPrime = new boolean[right + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
         for (int i = 2; i * i <= right; i++) {
-            if (sieve[i]) {
-                for (int j = i * i; j <= right; j += i) {
-                    sieve[j] = false;
+            if (isPrime[i]) {
+                for (int j = 2; i * j <= right; j++) {
+                    isPrime[i * j] = false;
                 }
             }
         }
-        
+        return isPrime;
+    }
+
+    public int[] closestPrimes(int left, int right) {
+        boolean[] isPrime = sieveHelper(right);
+
         List<Integer> primes = new ArrayList<>();
-        for (int i = left; i <= right; i++) {
-            if (sieve[i]) {
-                primes.add(i);
+        for (int num = left; num <= right; num++) {
+            if (isPrime[num]) {
+                primes.add(num);
             }
         }
-        
-        if (primes.size() < 2) {
-            return new int[]{-1, -1};
-        }
-        
-        int minGap = Integer.MAX_VALUE;
+
+        int minDiff = Integer.MAX_VALUE;
         int[] result = {-1, -1};
-        
+
         for (int i = 1; i < primes.size(); i++) {
-            int gap = primes.get(i) - primes.get(i - 1);
-            if (gap < minGap) {
-                minGap = gap;
+            int diff = primes.get(i) - primes.get(i - 1);
+            if (diff < minDiff) {
+                minDiff = diff;
                 result[0] = primes.get(i - 1);
                 result[1] = primes.get(i);
             }
         }
-        
+
         return result;
     }
 }
