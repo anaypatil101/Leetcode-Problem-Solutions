e2200a45df6a228cf0f9a408628e34e3bc6dfb9d
Anay Patil <73343302+Anay-Patil@users.noreply.github.com>
2025-06-01 01:20:40 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,19 @@
-//Recursion
+//Tabulation (space optimized)
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
-    }
 
-    private int solve(int n, int[] dp) {
         if(n <= 2)
             return n;
-        
-        if(dp[n] != -1)
-            return dp[n];
-        
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+        int a = 1, b = 2, c = -1;
+
+        for(int i=3; i<=n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
+
+    
 }
