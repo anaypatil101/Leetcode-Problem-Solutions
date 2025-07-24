ea02166d618f6b77511c0766c9207f5644054be6
Anay Patil <73343302+anaypatil101@users.noreply.github.com>
2025-07-24 19:10:36 +0530

    Time: 0 ms (100%), Space: 50.2 MB (65.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -5,26 +5,36 @@ class Solution {
         }
 
         int n = nums.length;
-        int count = 0;
-        int curr = -1;
-        
-        for(int num : nums) {
-            
-            if(curr != num) {
-                if(count == 1) {
-                    return curr;
-                }
-                else {
-                    curr = num;
-                    count = 1;
-                }
-            }
-            else {
-                count++;
-            }
-        }   
 
+        if(nums[0] != nums[1]) {
+            return nums[0];
+        }
+        else if(nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
 
-        return curr;
+        int low = 1, high = n - 2;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+            else if(nums[mid] == nums[mid - 1]) {
+                if(mid % 2 == 0)
+                    high = mid - 1;
+                else 
+                    low = mid + 1;
+            }
+            else if(nums[mid] == nums[mid + 1]) {
+                if(mid % 2 == 0) {
+                    low = mid + 1;
+                }
+                else 
+                    high = mid - 1;
+            }
+        }
+
+        return -1;
     } 
 }
