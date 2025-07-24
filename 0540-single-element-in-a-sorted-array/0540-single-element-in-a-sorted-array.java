d4d310749df170bc849d2d8fe273de760e97116f
Anay Patil <73343302+anaypatil101@users.noreply.github.com>
2025-07-24 18:57:13 +0530

    Time: 14 ms (5.01%), Space: 54.7 MB (5.47%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,32 +6,15 @@ class Solution {
 
         int n = nums.length;
 
-        if(nums[0] != nums[1]) {
-            return nums[0];
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for(int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
         }
-        else if(nums[n - 1] != nums[n - 2])
-            return nums[n - 1];
 
-        int low = 1, high = n - 2;
-
-        while(low <= high) {
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-                return nums[mid];
-            }
-            else if(nums[mid] == nums[mid - 1]) {
-                if(mid % 2 == 0)
-                    high = mid - 1;
-                else 
-                    low = mid + 1;
-            }
-            else if(nums[mid] == nums[mid + 1]) {
-                if(mid % 2 == 0) {
-                    low = mid + 1;
-                }
-                else 
-                    high = mid - 1;
+        for(Map.Entry<Integer,Integer> entry : frequency.entrySet()) {
+            if(frequency.get(entry.getKey()) == 1) {
+                return entry.getKey();
             }
         }
 
