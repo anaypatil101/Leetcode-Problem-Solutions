f473a2d7b7b2789ec71c11aa9a3722a6f44ad06b
Anay Patil <73343302+anaypatil101@users.noreply.github.com>
2025-07-31 23:53:47 +0530

    Time: 1 ms (51.44%), Space: 44.9 MB (60.81%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -3,26 +3,23 @@ class Solution {
 
         if(nums.length == 1) return nums;
 
-        Map<Integer, Integer> evenMap = new HashMap<>();
-        Map<Integer, Integer> oddMap = new HashMap<>();
-
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] % 2 == 0)
-                evenMap.put(i, nums[i]);
+        int n = nums.length;
+        int i = 0, j = n - 1;
+        //move i to the nearest odd element and j to nearest even
+        
+        while(i < j) {
+            if(nums[i] % 2 != 0 && nums[j] % 2 == 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++; j--;
+            }
+            else if(nums[i] % 2 == 0)
+                i++;
             else 
-                oddMap.put(i, nums[i]);
+                j--;
         }
-
-        int count = 0;
-        for(int a : evenMap.values()) {
-            nums[count] = a;
-            count++;
-        }
-        for(int a : oddMap.values()) {
-            nums[count] = a;
-            count++;
-        }
-
+        
         return nums;
     }
 }
